5f56a30f992d9310c5c36f4847fee91bd0b10ef3
Kasimir Blust <mail@kasimir-blust.de>
2019-08-17 19:24:39 +0200

    - add basic npc behaviour

--- Pepe/Assets/Scripts/UI/Textbox.cs
+++ Pepe/Assets/Scripts/UI/Textbox.cs
@@ -6,9 +6,22 @@ using UnityEngine.UI;
 public class Textbox : MonoBehaviour
 {
     public Text text;
-
-    public void PlayText()
+    public Image image;
+    
+    public void PlayText(string line)
     {
+        Game.instance.refs.GetPlayer().GetComponent<PlayerMovement>().enabled = false;
         
+        image.enabled = true;
+        text.enabled = true;
+        text.text = line;
+    }
+
+    public void Hide()
+    {
+         Game.instance.refs.GetPlayer().GetComponent<PlayerMovement>().enabled = true;
+                
+         image.enabled = false;
+         text.enabled = false;
     }
 }
---
+++ Pepe/Assets/Scripts/World/NPC.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : Interaction
+{
+    public string[] lines;
+
+    private int currentLine;
+    
+    public override void OnInteraction()
+    {
+        if (currentLine < lines.Length)
+        {
+            Game.instance.refs.GetTextbox().PlayText(lines[currentLine]);
+            currentLine++;
+        }
+        else
+        {
+            Game.instance.refs.GetTextbox().Hide();
+            currentLine = 0;
+        }
+    }
+}
