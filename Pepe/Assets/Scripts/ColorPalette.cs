09aa574c6d625fc5dd61b7cf63a1089e8394fb40
Erik Schulze <erisch01@web.de>
2019-08-17 13:04:03 +0200

    color palettes

---
+++ Pepe/Assets/Scripts/ColorPalette.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ColorPalette", fileName = "ColorPalette", order = 0)]
+public class ColorPalette : ScriptableObject
+{
+    public List<Color> colors;
+}
