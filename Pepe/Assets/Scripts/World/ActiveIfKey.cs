9e83f027564f340865f1410ba49ace3cb5fdbc81
Erik Schulze <erisch01@web.de>
2019-08-18 18:52:17 +0200

    ActiveIfKey to deactivate GameObjects

---
+++ Pepe/Assets/Scripts/World/ActiveIfKey.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveIfKey : MonoBehaviour
+{
+    public string key;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Game.instance.keys.HasKey(key))
+            gameObject.SetActive(true);
+        else
+            gameObject.SetActive(false);
+    }
+}
