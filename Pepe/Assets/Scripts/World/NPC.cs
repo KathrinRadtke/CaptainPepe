12f4ba62171e41dd93f0ce0b4219e235c1b5b8b1
Kathrin Radtke <kathrin.radtke@web.de>
2019-08-18 14:08:35 +0200

    -enable and disable objects

--- Pepe/Assets/Scripts/World/NPC.cs
+++ Pepe/Assets/Scripts/World/NPC.cs
@@ -5,20 +5,45 @@ using UnityEngine;
 public class NPC : Interaction
 {
     public string[] lines;
-
+    public GameObject[] objects;
     private int currentLine;
     
     public override void OnInteraction()
     {
         if (currentLine < lines.Length)
         {
-            Game.instance.refs.GetTextbox().PlayText(lines[currentLine]);
-            currentLine++;
+            if (lines[currentLine].StartsWith("*"))
+            {
+                string operation = lines[currentLine].Substring(1).Split(' ')[0];
+                string objectIndex = lines[currentLine].Split(' ')[1];
+
+
+                EnableObject(operation == "enable", int.Parse(objectIndex));
+                currentLine++;
+                OnInteraction();
+            }
+            else if(lines[currentLine].StartsWith("["))
+            {
+                // TODO: add or remove object from inventory
+                Debug.Log(lines[currentLine]);
+                currentLine++;
+                OnInteraction();
+            }
+            else
+            {
+                Game.instance.refs.GetTextbox().PlayText(lines[currentLine]);
+                currentLine++;
+            }
         }
         else
         {
             Game.instance.refs.GetTextbox().Hide();
             currentLine = 0;
         }
+    }
+
+    private void EnableObject(bool enable, int objectIndex)
+    {
+        objects[objectIndex].SetActive(enable);
     }
 }
